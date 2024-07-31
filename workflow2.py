bd6cc7ee27bf0d621b5ad91f0d3626bb53fb3fbc
karl kennedy <karl.j.kennedy@gsk.com>
2024-07-31 14:14:40 -0700

    Pushed from Domino: https://gskflows.cs.domino.tech/workspace/karl.j.kennedy/CDISC01_RE_KARL?executionId=668fbcb8e651b51a171f2895

--- workflow2.py
+++ workflow2.py
@@ -1,6 +1,8 @@
-import os
 from flytekit import workflow
-from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
+from flytekit.types.file import FlyteFile
+from typing import TypeVar, NamedTuple
+from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 
 
 # pyflyte run --remote workflow2.py POC_HW --sdtm_data_path /mnt/imported/data/SDTMBLIND/JUNE242024
@@ -12,8 +14,9 @@ def POC_HW(sdtm_data_path: str):
         flyte_task_name="ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_data_path", type=str, value=sdtm_data_path)],
-        output_spec=[Output(name="ADSL Dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="ADSL Dataset", type=FlyteFile[TypeVar("sas7bdat")])],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
-        dataset_snapshot=[DatasetSnapshot(Id="6679f7650d33391416fd83bc", Version=1)]
+        dataset_snapshots=[DatasetSnapshot(Id="6679f7650d33391416fd83bc", Version=1 )]
+        #dataset_snapshots=[DatasetSnapshot(Version=1,Name="JUNE242024" )]
     )
