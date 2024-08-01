23c52455697e9e1f892ec3eb17d299a6782e0000
karl kennedy <karl.j.kennedy@gsk.com>
2024-08-01 02:08:29 -0700

    Pushed from Domino: https://gskflows.cs.domino.tech/workspace/karl.j.kennedy/CDISC01_RE_KARL?executionId=668fbcb8e651b51a171f2895

--- workflow2.py
+++ workflow2.py
@@ -6,17 +6,21 @@ from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, 
 
 
 # pyflyte run --remote workflow2.py POC_HW --sdtm_data_path /mnt/imported/data/SDTMBLIND/JUNE242024
+# pyflyte run --remote workflow2.py POC_HW --sdtm_data_path /mnt/code/SDTMBLIND
 
 @workflow
 def POC_HW(sdtm_data_path: str):   
     # Create ADSL dataset
     data_prep_results = run_domino_job_task(
-        flyte_task_name="ADSL Dataset",
+        flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_data_path", type=str, value=sdtm_data_path)],
         output_specs=[Output(name="ADSL Dataset", type=FlyteFile[TypeVar("sas7bdat")])],
         use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        dataset_snapshots=[DatasetSnapshot(Id="6679f7650d33391416fd83bc", Version=1 )]
+        environment_name="SAS Analytics Pro"
+        #dataset_snapshots=[DatasetSnapshot(Id="6679f7650d33391416fd83bc", Version=1 )]
         #dataset_snapshots=[DatasetSnapshot(Version=1,Name="JUNE242024" )]
     )
+
+
+    
