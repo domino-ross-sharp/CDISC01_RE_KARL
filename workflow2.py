cc4d2fd77dd02543b5c376a4b08af288a4e44402
Ross Sharp <ross.sharp@dominodatalab.com>
2024-08-07 15:27:06 -0700

    Pushed from Domino: https://gskflows.cs.domino.tech/workspace/ross_domino/CDISC01_RE_KARL?executionId=66b3f288e651b51a171f2b80

--- workflow2.py
+++ workflow2.py
@@ -5,7 +5,6 @@ from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 
 
-# pyflyte run --remote workflow2.py ADAMS --sdtm_data_path /mnt/imported/data/SDTMBLIND/JUNE242024
 # pyflyte run --remote workflow2.py ADAMS --sdtm_data_path /mnt/data/snapshots/SDTMBLIND/1
 
 @workflow
@@ -15,11 +14,11 @@ def ADAMS(sdtm_data_path: str) -> FlyteFile[TypeVar("sas7bdat")]:
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_data_path", type=str, value=sdtm_data_path)],
-        output_specs=[Output(name="adslDataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adsl", type=FlyteFile[TypeVar("sas7bdat")])],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
-        dataset_snapshots=[DatasetSnapshot(Id="66abdd45e651b51a171f2a4a", Version=1 )]
+        dataset_snapshots=[DatasetSnapshot(Id="66b3f3a7e651b51a171f2b9c", Version=1 )]
     )
-    return adsl
+    return adsl["adsl"]
 
     
